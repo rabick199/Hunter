80b929504593c1d7d5a0b321985b81ec9db69ebd
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-02-22 16:08:31 +0530

    Create 6_1-repeated.c

---
+++ 6_1-repeated.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i,j,count=0;
+    printf("Enter the number N:");
+    scanf("%d",&n);
+    printf("\n Enter the array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\n The first repeated Numbers are:");
+    for(i=0;i<n-1;i++)
+    {
+        count=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+            }
+        }
+        if(count!=0)
+        {
+           printf("%d\t",a[i]);
+           break;
+        }
+    }
+}
